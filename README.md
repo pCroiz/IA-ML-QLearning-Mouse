6270855accee40c0265a73048efd98d586589bd2
Paul <paul@mac.local>
2024-07-23 18:02:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This work is based on this article : https://www.samyzaf.com/ML/rl/qmaze.html
+And this wikipedia page : https://fr.wikipedia.org/wiki/Q-learning
---
+++ main.py
@@ -0,0 +1,18 @@
+# List of the action (encoded by a number)
+LEFT = 0
+UP = 1
+RIGHT = 2
+DOWN = 3
+
+actions_dict = {
+    LEFT: 'left',
+    UP: 'up',
+    RIGHT: 'right',
+    DOWN: 'down'
+}
+
+# List of recompense
+move = 0.04
+get_cheese = 1.0
+go_wall = 0.75
+go_outside = 0.8
